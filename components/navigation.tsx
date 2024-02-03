f3a9df228db8d7429b6b5c963debf3cb3e423f70
Diego Borigen <diego@iddigital.com.au>
2024-02-03 23:22:33 +1100

    Mobile menu

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -3,7 +3,8 @@ import React, { useState, useEffect } from "react";
 import Image from "next/image";
 
 const Navigation = () => {
-    const [showNav, setShowNav] = useState(false);
+    const [showNav, setShowNav] = useState<boolean>(false);
+    const [openMobileNav, setOpenMobileNav] = useState<boolean>(false);
 
     useEffect(() => {
         const timer = setTimeout(() => {
@@ -40,6 +41,45 @@ const Navigation = () => {
             <button className={'btn-primary hidden md:flex'}>
                 Contact us
             </button>
+            <svg className={'h-[3rem]'} onClick={() => setOpenMobileNav(true)} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g id="SVGRepo_iconCarrier">
+                    <path d="M4 6H20M4 12H20M4 18H20" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    </path>
+                </g>
+            </svg>
+            <ul className={`${openMobileNav ? ' translate-x-[-100%]' : ''} p-4 transition-all duration-200 absolute h-screen w-[90vw] bg-white top-0 left-[100%]`}>
+                <li className={'mb-4'}>
+                    <svg className={'ml-auto'} onClick={() => setOpenMobileNav(false)} viewBox="0 0 24 24" width={40} height={40} fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g id="SVGRepo_iconCarrier">
+                            <path d="M19 5L4.99998 19M5.00001 5L19 19" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                        </g>
+                    </svg>
+                </li>
+                <li>
+                    <Image
+                        alt={'RockSteady Logo'}
+                        src={'/assets/logo.png'}
+                        width={118}
+                        height={64}
+                        className={'m-auto mb-10'}
+                    />
+                </li>
+                <li className={'my-5'}>
+                    <a className={'text-text-primary text-2xl'} href={'#section-services'}>Services</a>
+                </li>
+                <li className={'my-5'}>
+                    <a className={'text-text-primary text-2xl'} href={'#section-how-works'}>How</a>
+                </li>
+                <li className={'my-5'}>
+                    <a className={'text-text-primary text-2xl'} href={'#section-values'}>Values</a>
+                </li>
+                <li className={'my-5'}>
+                    <a className={'text-text-primary text-2xl'} href={'#section-mission'}>About</a>
+                </li>
+                <button className={'btn-primary flex m-auto w-full mt-6'}>
+                    Contact us
+                </button>
+            </ul>
         </nav>
     )
 };
