1d2c96b48ed14a7bc5c4c4c039a2353c6b532364
Diego Borigen <diego@iddigital.com.au>
2024-02-20 23:27:04 +1100

    Update hero video link

--- components/VideoBanner.tsx
+++ components/VideoBanner.tsx
@@ -74,7 +74,7 @@ const VideoBanner = () => {
                 className={'object-cover w-full md:h-full h-[45%] my-auto'}
                 onEnded={handleVideoEnd}
             >
-                <source src="https://res.cloudinary.com/du31j65g6/video/upload/v1703040232/new-branding-video-comp_kx414d.mp4" type="video/mp4" />
+                <source src="https://res.cloudinary.com/du31j65g6/video/upload/v1708431887/Rock%20Steady/hero-video-2-comp_lrnmq4.mp4" type="video/mp4" />
             </video>
             <a
                 href={'/#section-services'}
