50a175e274c168de59acbfbe3f51fa92ac205b49
Diego Borigen <diego@iddigital.com.au>
2024-03-02 21:51:13 +1100

    Mobile fixes

--- components/Values.tsx
+++ components/Values.tsx
@@ -7,8 +7,8 @@ const Values = () => {
     const gliderRef = useRef(null);
 
     return (
-        <section id={"section-values"} className={'min-h-screen w-full bg-texture bg-cover bg-no-repeat p-10 flex flex-col'}>
-            <div className={'flex items-center justify-center 2xl:mb-10 md:mb-4'}>
+        <section id={"section-values"} className={'md:min-h-screen w-full bg-texture bg-cover bg-no-repeat p-10 flex flex-col'}>
+            <div className={'flex items-center justify-center 2xl:mb-10 md:mb-4 mb-10'}>
                 <svg width="28" height="25" viewBox="0 0 28 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M14.3714 1L13.1333 9.16129L1 11.8817V13.3656L13.1333 16.3333L14.3714 24L16.1048 16.3333L27 13.3656V11.8817L16.1048 9.16129L14.3714 1Z" fill="#080D26" stroke="black"/>
                 </svg>
@@ -20,7 +20,7 @@ const Values = () => {
                     <path d="M14.3714 1L13.1333 9.16129L1 11.8817V13.3656L13.1333 16.3333L14.3714 24L16.1048 16.3333L27 13.3656V11.8817L16.1048 9.16129L14.3714 1Z" fill="#080D26" stroke="black"/>
                 </svg>
             </div>
-            <div className={'hidden md:grid grid-cols-2 justify-center md:p-2 2xl:p-3 flex-grow gap-12'}>
+            <div className={'hidden md:grid grid-cols-2 justify-center md:p-2 2xl:p-3 flex-grow gap-4'}>
                 {
                     VALUES.map((v, index) => {
                         return (
@@ -31,7 +31,7 @@ const Values = () => {
                                 <h4 className={'text-3xl text-text-primary font-poppins-extrabold mb-3 text-center'}>
                                     {v.title}
                                 </h4>
-                                <p className={'text-center text-[14px]'}>
+                                <p className={'text-center'}>
                                     {v.description}
                                 </p>
                             </div>
